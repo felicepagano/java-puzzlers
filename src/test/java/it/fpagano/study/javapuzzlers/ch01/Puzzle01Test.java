d6b86f9ea628ae40a4d03ddbbc3f57a0d59115c1
Felice Pagano <felice.pagano@eidosmedia.com>
2019-10-21 12:53:33 +0200

    add displayname on tests

--- src/test/java/it/fpagano/study/javapuzzlers/ch01/Puzzle01Test.java
+++ src/test/java/it/fpagano/study/javapuzzlers/ch01/Puzzle01Test.java
@@ -1,31 +1,34 @@
 package it.fpagano.study.javapuzzlers.ch01;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
-
-import it.fpagano.study.javapuzzlers.ch01.Puzzle01;
-
-import static org.junit.jupiter.api.Assertions.*; 
+import org.junit.jupiter.api.DisplayName;
 
 public class Puzzle01Test {
 
-    @Test public void whenNubersArePositive_ThenFirstOddVersionMustWorks() {
+    @Test
+    @DisplayName("if you compare the remainder with 1 the function will not works for negative numbers")
+    public void whenNubersArePositive_ThenFirstOddVersionMustWorks() {
         assertTrue(Puzzle01.isOdd(3));
         assertFalse(Puzzle01.isOdd(2));
-    }
-
-    @Test public void whenNubersAreNegative_ThenFirstOddVersionMustNotWorks() {
         assertFalse(Puzzle01.isOdd(-3));
         assertFalse(Puzzle01.isOdd(-2));
     }
 
-    @Test public void fixedOddVersionMustWorksWithAllNumbers() {
+    @Test
+    @DisplayName("you should compare the remainder to be different from 0 in order to be able to use also negative numbers")
+    public void fixedOddVersionMustWorksWithAllNumbers() {
         assertTrue(Puzzle01.isOddWorkWithNegativeNumber(3));
         assertFalse(Puzzle01.isOddWorkWithNegativeNumber(2));
         assertTrue(Puzzle01.isOddWorkWithNegativeNumber(-3));
         assertFalse(Puzzle01.isOddWorkWithNegativeNumber(-2));
     }
 
-    @Test public void performanceFixedOddVersionMustWorksWithAllNumbers() {
+    @Test
+    @DisplayName("bitwise operation is more performant than division. you can check if a number is odd just checking the last bit")
+    public void performanceFixedOddVersionMustWorksWithAllNumbers() {
         assertTrue(Puzzle01.isOddPerformantVersion(3));
         assertFalse(Puzzle01.isOddPerformantVersion(2));
         assertTrue(Puzzle01.isOddPerformantVersion(-3));
--- src/test/java/it/fpagano/study/javapuzzlers/ch01/Puzzle02Test.java
+++ src/test/java/it/fpagano/study/javapuzzlers/ch01/Puzzle02Test.java
@@ -1,23 +1,32 @@
 package it.fpagano.study.javapuzzlers.ch01;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import java.math.BigDecimal;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class Puzzle02Test {
 
-    @Test public void whenSubtractTwoDouble_ThenPrecisionIsLost() {
+    @Test
+    @DisplayName("subtracting two doubles will cause lost precision") 
+    public void whenSubtractTwoDouble_ThenPrecisionIsLost() {
         assertNotEquals(0.9, Puzzle02.subtractTwoDouble(2.0, 1.1));
     }
 
 
-    @Test public void whenSubtractTwoInt_ThenPrecisionIsOK() {
+    @Test 
+    @DisplayName("integer or long could be a solution in order to perform a subtraction in cents")
+    public void whenSubtractTwoInt_ThenPrecisionIsOK() {
         assertEquals(90, Puzzle02.subtractTwoInt(200, 110));
     }
 
 
-    @Test public void whenSubtractTwobigDecimal_ThenPrecisionIsOK() {
+    @Test 
+    @DisplayName("BigDecimal is the right type in order to preserve precision. Use the string constructor")
+    public void whenSubtractTwobigDecimal_ThenPrecisionIsOK() {
         assertEquals(new BigDecimal("0.9"), Puzzle02.subtractBigDecimal(new BigDecimal("2.0"), new BigDecimal("1.1")));
     }
 }
